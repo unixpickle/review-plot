4aae1268a82fe8fb6d7239652ea2ab1d32330cb9
Alex Nichol <unixpickle@gmail.com>
2024-05-01 22:10:44 -0400

    page structure

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
-use std::{collections::HashMap, convert::Infallible, error::Error, fmt::Display, str::FromStr};
+use std::{convert::Infallible, error::Error};
 
 use bytes::Bytes;
 use clap::Parser;
-use futures::StreamExt;
+use futures::{pin_mut, select, FutureExt};
 use http::response::Builder;
-use http_body_util::{combinators::BoxBody, Full, StreamBody};
-use hyper::{
-    body::{self, Frame},
-    server::conn::http1,
-    service::service_fn,
-    Request, Response,
-};
+use http_body_util::{combinators::BoxBody, Full};
+use hyper::{body, server::conn::http1, service::service_fn, Request, Response};
 
 mod client;
 mod client_pool;
-use client::{Client, GeoLocation, LocationInfo, ScrapeError, SearchResult};
-use client_pool::{new_client_pool, ObjectPool, PoolError};
+mod handlers;
+use client::Client;
+use client_pool::{new_client_pool, ObjectPool};
+use handlers::{api_result_to_response, handle_reviews, handle_search};
 use hyper_util::rt::{TokioIo, TokioTimer};
-use serde::Serialize;
-use serde_json::json;
-use tokio::{net::TcpListener, sync::mpsc::channel};
-use tokio_stream::wrappers::ReceiverStream;
+use tokio::{net::TcpListener, signal};
+
+const PAGE_MAPPING: [(&'static str, &'static str); 8] = [
+    ("", include_str!("assets/index.html")),
+    ("/", include_str!("assets/index.html")),
+    ("/404.html", include_str!("assets/404.html")),
+    ("/js/app.js", include_str!("assets/js/app.js")),
+    ("/js/app.js.map", include_str!("assets/js/app.js.map")),
+    ("/ts/app.ts", include_str!("assets/ts/app.ts")),
+    ("/css/page.css", include_str!("assets/css/page.css")),
+    ("/css/404.css", include_str!("assets/css/404.css")),
+];
 
 #[derive(Parser, Clone)]
 #[clap(author, version, about, long_about = None)]
@@ -43,58 +48,24 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     result
 }
 
-#[derive(Debug)]
-enum HandlerError {
-    ScrapeError(ScrapeError),
-    PoolError(PoolError),
-    HttpError(http::Error),
-    QueryError(String),
-}
-
-impl Display for HandlerError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            HandlerError::ScrapeError(e) => write!(f, "ScrapeError({})", e),
-            HandlerError::PoolError(e) => write!(f, "PoolError({})", e),
-            HandlerError::HttpError(e) => write!(f, "HttpError({})", e),
-            HandlerError::QueryError(e) => write!(f, "QueryError({})", e),
-        }
-    }
-}
-
-impl From<ScrapeError> for HandlerError {
-    fn from(value: ScrapeError) -> Self {
-        HandlerError::ScrapeError(value)
-    }
-}
-
-impl From<PoolError> for HandlerError {
-    fn from(value: PoolError) -> Self {
-        HandlerError::PoolError(value)
-    }
-}
-
-impl From<http::Error> for HandlerError {
-    fn from(value: http::Error) -> Self {
-        HandlerError::HttpError(value)
-    }
-}
-
-impl From<url::ParseError> for HandlerError {
-    fn from(value: url::ParseError) -> Self {
-        HandlerError::QueryError(format!("failed to parse URL: {}", value))
-    }
-}
-
-impl Error for HandlerError {}
-
 async fn entrypoint(
     args: Args,
     pool: &ObjectPool<Client>,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     let listener = TcpListener::bind(&args.host).await?;
+    let exit_signal = signal::ctrl_c().fuse();
+    pin_mut!(exit_signal);
     loop {
-        let (tcp, _) = listener.accept().await?;
+        let tcp;
+        let accept = listener.accept().fuse();
+        pin_mut!(accept);
+        select! {
+            x = accept => tcp = x?.0,
+            _ = exit_signal => {
+                println!("Got interrupt; stopping server.");
+                return Ok(());
+            }
+        }
         let io = TokioIo::new(tcp);
 
         let local_pool = pool.clone();
@@ -113,9 +84,14 @@ async fn entrypoint(
                         Ok(x) => Ok(x),
                     }
                 } else {
+                    for (page, content) in PAGE_MAPPING {
+                        if req.uri().path() == page {
+                            return Ok(static_response(Response::builder(), content)?);
+                        }
+                    }
                     Ok(static_response(
                         Response::builder().status(404),
-                        "404 not found",
+                        include_str!("assets/404.html"),
                     )?)
                 }
             }
@@ -130,157 +106,6 @@ async fn entrypoint(
                 println!("Error serving connection: {:?}", err);
             }
         });
-    }
-
-    // let mut client = pool.get().await;
-    // let location = GeoLocation {
-    //     latitude: 37.63,
-    //     longitude: -122.44,
-    //     accuracy: 10.0,
-    // };
-    // let results = client.search("Death by Taco", &location).await?;
-    // println!("got results: {:?}", results);
-    // match results {
-    //     SearchResult::Singular(x) => {
-    //         println!("getting reviews for {}", x.name);
-    //         let mut count = 0;
-    //         let mut review_it = client.list_reviews(&x.url, &location).await?;
-    //         while let Some(result) = review_it.next().await? {
-    //             println!("{:?}", result);
-    //             count += result.len();
-    //             println!("seen {} results so far", count);
-    //         }
-    //     }
-    //     _ => {}
-    // }
-    // Ok(())
-}
-
-async fn handle_search(
-    pool: ObjectPool<Client>,
-    request: Request<body::Incoming>,
-) -> Result<Vec<LocationInfo>, HandlerError> {
-    let args = Query::parse(&request)?;
-
-    let client = pool.get().await?;
-    let location = GeoLocation {
-        latitude: args.get("latitude")?,
-        longitude: args.get("longitude")?,
-        accuracy: args.get("accuracy")?,
-    };
-    Ok(
-        match client
-            .search(&args.get::<String>("query")?, &location)
-            .await?
-        {
-            SearchResult::NotFound => vec![],
-            SearchResult::Singular(x) => vec![x],
-            SearchResult::Multiple(x) => x,
-        },
-    )
-}
-
-async fn handle_reviews(
-    pool: ObjectPool<Client>,
-    request: Request<body::Incoming>,
-) -> Result<Response<BoxBody<Bytes, Infallible>>, HandlerError> {
-    let args = Query::parse(&request)?;
-
-    let location = GeoLocation {
-        latitude: args.get("latitude")?,
-        longitude: args.get("longitude")?,
-        accuracy: args.get("accuracy")?,
-    };
-    let url = args.get::<String>("url")?;
-    let mut client = pool.get().await?;
-
-    let (tx, rx) = channel::<Bytes>(1);
-
-    tokio::spawn(async move {
-        match client.list_reviews(&url, &location).await {
-            Err(e) => {
-                tx.send(Bytes::from(
-                    serde_json::to_string(&json!({"error": format!("{}", e)})).unwrap(),
-                ))
-                .await
-                .ok();
-            }
-            Ok(mut it) => loop {
-                match it.next().await {
-                    Err(e) => {
-                        tx.send(Bytes::from(
-                            serde_json::to_string(&json!({"error": format!("{}", e)})).unwrap(),
-                        ))
-                        .await
-                        .ok();
-                        return;
-                    }
-                    Ok(Some(x)) => {
-                        if !tx
-                            .send(Bytes::from(serde_json::to_string(&x).unwrap()))
-                            .await
-                            .is_ok()
-                        {
-                            return;
-                        }
-                    }
-                    Ok(None) => return,
-                }
-            },
-        }
-    });
-
-    Ok(Response::new(BoxBody::new(StreamBody::new(
-        ReceiverStream::from(rx).map(|x| -> Result<_, Infallible> { Ok(Frame::data(x)) }),
-    ))))
-}
-
-struct Query {
-    map: HashMap<String, String>,
-}
-
-impl Query {
-    fn parse(request: &Request<body::Incoming>) -> Result<Self, HandlerError> {
-        let query = request
-            .uri()
-            .query()
-            .ok_or_else(|| HandlerError::QueryError("missing query string".to_owned()))?;
-        let mut value = HashMap::new();
-        for (k, v) in url::form_urlencoded::parse(query.as_bytes()) {
-            value.insert(k.into(), v.into());
-        }
-        Ok(Self { map: value })
-    }
-
-    fn get<T: FromStr>(&self, k: &str) -> Result<T, HandlerError>
-    where
-        T::Err: Display,
-    {
-        if let Some(val) = self.map.get(k) {
-            T::from_str(val).map_err(|x| {
-                HandlerError::QueryError(format!("failed to parse argument {}: {}", k, x))
-            })
-        } else {
-            Err(HandlerError::QueryError(format!("no argument: {}", k)))
-        }
-    }
-}
-
-fn api_result_to_response<T: Serialize, E: Error + Display>(
-    builder: Builder,
-    result: Result<T, E>,
-) -> Result<Response<BoxBody<Bytes, Infallible>>, http::Error> {
-    match result {
-        Ok(x) => match serde_json::to_string(&x) {
-            Ok(x) => builder.body(BoxBody::new(Full::<Bytes>::from(x))),
-            Err(x) => builder.status(500).body(BoxBody::new(Full::<Bytes>::from(
-                serde_json::to_string(&json!({"error": format!("failed to encode result: {}", x)}))
-                    .unwrap(),
-            ))),
-        },
-        Err(x) => builder.body(BoxBody::new(Full::<Bytes>::from(
-            serde_json::to_string(&json!({"error": format!("{}", x)})).unwrap(),
-        ))),
     }
 }
 
