f1dfdb504e84460a20a61d26b105822f9df6ba5d
Alex Nichol <unixpickle@gmail.com>
2024-05-13 23:57:31 -0400

    location picker

--- src/assets/ts/location.ts
+++ src/assets/ts/location.ts
@@ -1,12 +1,29 @@
 class LocationPicker {
     private _element: HTMLElement;
+    private collapseButton: HTMLButtonElement;
     private latitude: HTMLInputElement;
     private longitude: HTMLInputElement;
     private accuracy: HTMLInputElement;
 
     constructor() {
         this._element = document.createElement('div');
-        this._element.className = 'location-picker';
+        this._element.className = 'location-picker location-picker-closed';
+
+        const header = document.createElement('div');
+        header.className = 'location-picker-header';
+        this.collapseButton = document.createElement('button');
+        this.collapseButton.addEventListener('click', () => {
+            this._element.classList.toggle('location-picker-closed');
+        })
+        header.appendChild(this.collapseButton);
+        const label = document.createElement('label');
+        label.textContent = 'Search from geolocation';
+        label.addEventListener('click', () => {
+            this._element.classList.toggle('location-picker-closed');
+        })
+        header.appendChild(label);
+        this._element.appendChild(header);
+
         this.latitude = createLabeledInput(this._element, 'Lat', '37.63');
         this.longitude = createLabeledInput(this._element, 'Lon', '-122.44');
         this.accuracy = createLabeledInput(this._element, 'Acc', '10.0');
